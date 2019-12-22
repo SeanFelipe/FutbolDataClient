d052124c84da87eeb0a701412d67149920a978f5
SeanFelipe <sean@addlightness.tech>
2019-12-22 14:33:45 -0800

    okay running a browser with jasmine now !!

--- spec/JubulaSpec.js
+++ spec/JubulaSpec.js
@@ -1,6 +1,13 @@
+const { Builder, By, Key, until } = require('selenium-webdriver')
+
+const driver = new Builder().forBrowser('chrome').build()
+const base = 'http://localhost:3000'
+
 describe("Splash Page", function() {
   it("starts", async function() {
     console.log("starting Splash Page test")
+
+    await driver.get(base)
   })
   /*
   beforeEach(function() {
