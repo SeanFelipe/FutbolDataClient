4a5d3cf66da92b544ef7b79fad9c99bed3cfc1b9
SeanFelipe <sean@addlightness.tech>
2019-12-22 09:35:15 -0800

    okay have a basic Jasmine setup working !!

---
+++ spec/JubulaSpec.js
@@ -0,0 +1,21 @@
+describe("Splash Page", function() {
+  it("starts", async function() {
+    console.log("starting Splash Page test")
+  })
+  /*
+  beforeEach(function() {
+
+  var Player = require('../../lib/jasmine_examples/Player');
+  var Song = require('../../lib/jasmine_examples/Song');
+  var player;
+  var song;
+
+    it("should be able to play a Song", function() {
+    player.play(song);
+    expect(player.currentlyPlayingSong).toEqual(song);
+
+    //demonstrates use of custom matcher
+    expect(player).toBePlaying(song);
+  });
+  */
+})
