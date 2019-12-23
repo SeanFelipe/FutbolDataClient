d974e9e26324ab51e0fc1753ea55a7d387cb579d
SeanFelipe <sean@addlightness.tech>
2019-12-22 17:25:57 -0800

    remove karma stuffs, need wdio, making progress

--- spec/JubulaSpec.js
+++ spec/JubulaSpec.js
@@ -8,21 +8,7 @@ describe("Splash Page", function() {
     console.log("starting Splash Page test")
 
     await driver.get(base)
+    driver.debug()
+
   })
-  /*
-  beforeEach(function() {
-
-  var Player = require('../../lib/jasmine_examples/Player');
-  var Song = require('../../lib/jasmine_examples/Song');
-  var player;
-  var song;
-
-    it("should be able to play a Song", function() {
-    player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
-
-    //demonstrates use of custom matcher
-    expect(player).toBePlaying(song);
-  });
-  */
 })
